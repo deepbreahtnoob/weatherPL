7ce35739f49ad41c2b379d6be72390064370ebd7
deepbreahnoob <kuciel.pawel@gmail.com>
2016-12-18 22:42:33 +0100

    web interface created, blade thame engine add.

--- weather.php
+++ weather.php
@@ -2,7 +2,13 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use Jkanclerz\Weather\CurrentWeather;
+use DeepBreath\Weather\CurrentWeather;
+use Philo\Blade\Blade;
+
+$views = __DIR__.'/views';
+$cache = __DIR__.'/cache';
+$blade = new Blade($views, $cache);
+echo $blade->view()->make('hello')->render();
 
 $city = $argv[1];
 $apiKey = 'af319cd969dff7d8c42768f6f0d8c979';
